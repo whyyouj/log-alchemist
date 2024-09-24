15fa6251210061f0065478f454e70ec3e2e72a67
a122882 <phojiayuan@gmail.com>
2024-09-25 00:55:54 +0800

    new

--- agent_v0/setup.py
+++
@@ -1,9 +0,0 @@
-import setuptools
-from setuptools import setup
-
-setup(name= "llm",
-      verions = 1,
-      author = "25",
-      license = "Apache License 2.0",
-      package_dir={"":"version1"},
-      packages = setuptools.find_namespace_packages(where="./version1"))
