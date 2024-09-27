581da7b65f24a1aad15dcafba1a5c8f2d1794251
a122882 <phojiayuan@gmail.com>
2024-09-27 13:29:35 +0800

    add try again ability

--- version2/version_temp/lang_graph/lang_graph.py
+++ version2/version_temp/lang_graph/lang_graph.py
@@ -4,6 +4,7 @@ import operator
 from langgraph.graph import StateGraph, END
 import pandas as pd
 from python_agent.python_ai import Python_Ai
+from regular_agent.agent_ai import Agent_Ai
 from lang_graph.lang_graph_utils import start_agent, python_pandas_ai, final_agent, router    
 
 class AgentState(TypedDict):
@@ -17,6 +18,7 @@ class Graph:
     def __init__(self, pandas_llm, df):
         self.pandas = pandas_llm
         self.df = df
+        self.qns=''
     
     def get_graph(self):
         graph = StateGraph(AgentState)
@@ -39,6 +41,24 @@ class Graph:
         return runnable
     
     def run(self, query):
+        llm = Agent_Ai(model='mistral', temperature=0)
+        prompt = f"""The user has asked: '{query}'.
+        Determine if this question is asking to "try again", "retry", or something with a similar meaning related to repeating an action.
+        If the question is about retrying, respond with 'Yes'.
+        If the question is not about retrying, respond with 'No'.
+        Only answer 'Yes' or 'No'"""
+        ans = llm.query_agent(prompt)
+        print(f'Try again agent: {ans}')
+        if 'yes' in ans.lower():
+            if self.qns != '':
+                query = self.qns
+            else:
+                option = ['😀','🥳','😊','🥳','🤩','😎','😄','🤭']
+                import numpy as np
+                num = np.random.randint(0,len(option)-1)
+                return f"Hi! Please ask a question {option[num]}"
+        else:
+            self.qns = query
         runnable = self.get_graph()
         out = runnable.invoke({"input":f"{query}", "pandas":self.pandas, "df":self.df})
         return out['agent_out']
