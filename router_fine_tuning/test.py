3264e83b9b4c9efc3ada8dd552c338e001a9a46f
a122882 <phojiayuan@gmail.com>
2024-10-19 14:56:58 +0800

    update .gitignore

--- router_fine_tuning/test.py
+++ router_fine_tuning/test.py
@@ -29,38 +29,40 @@ questions = ["explain why there are many peopple in the world"]
 # df = pd.read_csv("../../data/Mac_2k.log_structured.csv")
 # questions2 =["how many rows are there"]
 
-llm = Agent_Ai(model = 'jiayuan1/summary_anomaly_llm')
+llm = Agent_Ai(model = 'jiayuan1/summary_anomaly_llm_v2')
 df = pd.read_csv('train2.csv')
-llm = Agent_Ai(model = 'jiayuan1/summary_anomaly_llm', df = df)
+# llm = Agent_Ai(model = 'jiayuan1/summary_anomaly_llm', df = df)
 # llm= Python_Ai(model='mistral', df = df).pandas_legend_with_summary_skill()
-# correct = 0
-# total = 0
-# df = pd.read_csv('train2.csv')
-# for i in range(len(df)):
-#     # print(i)
-#     #print(llm.chat(i))
-#     print(i)
-#     out = llm.query_agent(f'What category does this question fall under: Summary, Anomaly, General:\n question: summary {df.iloc[i].Input}')
-#     total += 1
-#     if out == df.iloc[i].Output:
-#         print(out)
-#         correct += 1
+correct = 0
+total = 0
+df = pd.read_csv('train2.csv')
+for i in range(len(df)):
+    # print(i)
+    #print(llm.chat(i))
+    print(i)
+    out = llm.query_agent(f'What category does this question fall under: Summary, Anomaly, General:\n Question: {df.iloc[i].Input} \n Only output your Response')
+    # out = llm.query_agent(df.iloc[i].Input)
+    total += 1
+
+    if out == df.iloc[i].Output:
+        print(out)
+        correct += 1
         
-# print(correct)
-# print(total)
-#1570/1734
+print(correct)
+print(total)
+#1662/1734
 
 #out = llm.query_agent('What category does this question fall under: Summary, Anomaly, General: \nSummarise the data')
 # from langchain_core.prompts import PromptTemplate
 # prompt = PromptTemplate.from_template('What category does this question fall under: Summary, Anomaly, General:\n Question: summary of the data')
 
-# out = llm.query_agent(query='What category does this question fall under: Summary, Anomaly, General:\n question: summary')
+# out = llm.query_agent(query='what the people say')
 # print(out)
 
-for i in questions:
-    # print(i)
-    #print(llm.chat(i))
+# for i in questions:
+#     # print(i)
+#     #print(llm.chat(i))
     
-    out = llm.query_agent(f'What category does this question fall under: Summary, Anomaly, General: \n question: {i}')
-    print(i)
-    print(out)
+#     out = llm.query_agent(f'What category does this question fall under: Summary, Anomaly, General: \n question: {i}')
+#     print(i)
+#     print(out)
